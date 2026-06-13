dfa131a3a64f08f4f6af638beb0cbfe16a3c5d05
Egor Kirnosov <kirnosov.eg@yandex.ru>
2026-06-13 16:01:50 +0300

    bot_service: add jwt.py

---
+++ bot_service/app/core/jwt.py
@@ -0,0 +1,23 @@
+from jose import jwt, JWTError, ExpiredSignatureError
+
+from app.core.config import settings
+
+
+def decode_and_validate(token: str) -> dict:
+    try:
+        payload = jwt.decode(
+            token,
+            settings.JWT_SECRET,
+            algorithms=[settings.JWT_ALG],
+        )
+
+        if "sub" not in payload:
+            raise ValueError("Invalid token: missing sub")
+
+        return payload
+
+    except ExpiredSignatureError:
+        raise ValueError("Token expired")
+
+    except JWTError:
+        raise ValueError("Invalid token")
