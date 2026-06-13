7abfe5f22ff96bd0f21191e3df15b436758f46b2
Egor Kirnosov <kirnosov.eg@yandex.ru>
2026-06-13 13:31:08 +0300

    auth service: update config.py

--- auth_service/app/core/config.py
+++ auth_service/app/core/config.py
@@ -9,7 +9,7 @@ class Settings(BaseSettings):
     JWT_ALG: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60
 
-    SQLITE_PATH: str = "./auth.db"
+    SQLITE_PATH: str
 
     model_config = SettingsConfigDict(
         env_file=".env",
