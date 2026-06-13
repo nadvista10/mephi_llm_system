f3b9ffaa87d9fdaf9447af27f651669ef656ae09
Egor Kirnosov <kirnosov.eg@yandex.ru>
2026-06-13 13:26:41 +0300

    auth service: add base impl:  main, router, config

---
+++ auth_service/app/core/config.py
@@ -0,0 +1,20 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    APP_NAME: str = "auth-service"
+    ENV: str = "local"
+
+    JWT_SECRET: str
+    JWT_ALG: str = "HS256"
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 60
+
+    SQLITE_PATH: str = "./auth.db"
+
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        extra="ignore",
+    )
+
+
+settings = Settings()
--- auth_service/app/main.py
+++ auth_service/app/main.py
@@ -1,6 +1,29 @@
-def main():
-    print("Hello from auth-service!")
+from contextlib import asynccontextmanager
+
+from fastapi import FastAPI
+
+from app.api.router import router
+from app.core.config import settings
 
 
-if __name__ == "__main__":
-    main()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    print("Auth Service started")
+    yield
+    print("Auth Service stopped")
+
+
+app = FastAPI(
+    title=settings.APP_NAME,
+    lifespan=lifespan,
+)
+
+app.include_router(router)
+
+
+@app.get("/health")
+async def health():
+    return {
+        "status": "ok",
+        "service": settings.APP_NAME,
+    }
