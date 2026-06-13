0bea7788efadb23736b3724c2cc8092e0f16a703
Egor Kirnosov <kirnosov.eg@yandex.ru>
2026-06-13 13:38:06 +0300

    auth service: add exceptions.py

---
+++ auth_service/app/core/exceptions.py
@@ -0,0 +1,53 @@
+from fastapi import HTTPException
+
+class BaseHTTPException(HTTPException):
+    def __init__(self, status_code: int, detail: str):
+        super().__init__(status_code=status_code, detail=detail)
+
+
+class UserAlreadyExistsError(BaseHTTPException):
+    def __init__(self):
+        super().__init__(
+            status_code=409,
+            detail="User already exists"
+        )
+
+
+class InvalidCredentialsError(BaseHTTPException):
+    def __init__(self):
+        super().__init__(
+            status_code=401,
+            detail="Invalid email or password"
+        )
+
+
+class InvalidTokenError(BaseHTTPException):
+    def __init__(self):
+        super().__init__(
+            status_code=401,
+            detail="Invalid token"
+        )
+
+
+class TokenExpiredError(BaseHTTPException):
+    def __init__(self):
+        super().__init__(
+            status_code=401,
+            detail="Token expired"
+        )
+
+
+class UserNotFoundError(BaseHTTPException):
+    def __init__(self):
+        super().__init__(
+            status_code=404,
+            detail="User not found"
+        )
+
+
+class PermissionDeniedError(BaseHTTPException):
+    def __init__(self):
+        super().__init__(
+            status_code=403,
+            detail="Permission denied"
+        )
